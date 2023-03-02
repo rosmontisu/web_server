66e25b86cfb82f672bd4141c7f9cc4058cfae52a
gghongsam <107000683+gghongsam@users.noreply.github.com>
2023-03-03 05:13:26 +0900

    save

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const express = require('express'); // 라이브러리 요청
-const app = express(); // 객체 생성
+const express = require('express');
+const app = express();
+const app2 = express();
+
+//1. listen
+//2. get(req, res)
 
 app.listen(8080, function(){
     console.log('listening on 8080')
-}); // 서버 열기(port, 서버 띄운 후 실행할 코드)
+});
+
+app.get('/test', function(req, res){
+    res.send('/test에 응답 페이지')
+});
+
+
+app2.listen(5050, function(){
+    console.log('5050')
+});
+
+app2.get('/test', function(req, res){
+    res.send('app2 /test res')
+});
