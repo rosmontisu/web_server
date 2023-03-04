e2fd6b50a6e2a9449192f59ee3ae9bd2e9b00b0d
gghongsam <107000683+gghongsam@users.noreply.github.com>
2023-03-04 16:48:46 +0900

    .

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const app2 = express();
 
 //1. listen
 //2. get(req, res)
@@ -9,15 +8,12 @@ app.listen(8080, function(){
     console.log('listening on 8080')
 });
 
+
+
 app.get('/test', function(req, res){
     res.send('/test에 응답 페이지')
-});
+}); 
 
-
-app2.listen(5050, function(){
-    console.log('5050')
-});
-
-app2.get('/test', function(req, res){
-    res.send('app2 /test res')
-});
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+}); 
